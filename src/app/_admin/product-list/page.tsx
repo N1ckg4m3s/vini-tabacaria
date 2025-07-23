55edbe36410f93653adee8ea0c22f54624515c6a
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-07-23 19:37:47 -0300

    [ INIT ] Admin pages

---
+++ src/app/_admin/product-list/page.tsx
@@ -0,0 +1,12 @@
+import * as s from './style';
+
+const _AdmProductListPage = () => {
+    return (
+        <div>
+            <h1>Admin Lista de produtos</h1>
+            {/* Add your login form here */}
+        </div>
+    );
+}
+
+export default _AdmProductListPage;

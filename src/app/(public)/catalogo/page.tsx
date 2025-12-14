80d485a5f5f35b522b5d39b54a18200e6bbeeacb
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-12-14 12:07:43 -0300

    [ FIX ] Alterando o catalogo para gerar a pagina do Feature

--- src/app/(public)/catalogo/page.tsx
+++ src/app/(public)/catalogo/page.tsx
@@ -1,35 +1,6 @@
-'use client'
-import CatalogoComponent from "./catalogo/catalogo";
-import FiltroCatalogoComponent from "./filtro-para-o-catalogo/filter";
-import { useParams } from "next/navigation";
-import { ContainerPagina } from "@/styles/mixins";
+import { CatalogoComponent } from "@/features/catalog/pages/catalogo.page"
 
-/**
- * Essa pagina tem como foco:
- * - Exibição do conteudo
- * - Organização dos componentes
-*/
-const Catalogo = () => {
-    /**
-     * Parametros passados pela URL
-     * 
-     * filtro: identificador de esoecificação do produto:
-     *  - Essencia
-     *  - Carvão e Aluminio
-     *  - Acessorio
-     * 
-     * search: Pesquisa atual do usuario.
-    */
-    const { filtro, search } = useParams();
-
-    return (
-        <ContainerPagina>
-            <FiltroCatalogoComponent
-                filter={(filtro ?? '').toString()}
-            />
-            <CatalogoComponent />
-        </ContainerPagina>
-    );
-};
-
-export default Catalogo;
+// PAGINA
+export default () => {
+    return <CatalogoComponent />
+}

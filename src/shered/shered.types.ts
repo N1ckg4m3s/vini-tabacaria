a8117a8e15b3e862160d210fe5d8dd110be4a730
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-12-14 12:36:18 -0300

    [ FEAT ] Adicionando tipagem compartilhada entre front e back

---
+++ src/shered/shered.types.ts
@@ -0,0 +1,56 @@
+// --------------------
+// Campos base nos produtos
+// --------------------
+export interface Produto {
+  id: string;
+  nome: string;
+  marca: string;
+  valor: number;
+  tipo: string;
+  imagem?: string
+  metadata: Record<string, string | number | string[] | number[]>
+}
+
+// --------------------
+// Formato dos filtros
+// --------------------
+
+export interface CatalogFilterSource {
+  marca?: string[]
+  tipo?: string[]
+  meta?: {
+    sabor?: string[]
+    intensidade?: string[]
+    cor?: string[]
+    tamanho?: string[]
+    kit?: string[]
+  }
+}
+
+export interface CatalogFilters {
+  search?: string
+
+  // sempre ARRAY
+  marca?: string[]
+  tipo?: string[]
+
+  // sempre NUMBER
+  precoMin?: number
+  precoMax?: number
+
+  meta?: {
+    sabor?: string[]
+    intensidade?: string[]
+    cor?: string[]
+    tamanho?: string[]
+    kit?: string[]
+  }
+}
+
+export const filtrosPorTipo = {
+  essencia: ["sabor", "intensidade", "mix", "tipo"],
+  acessorio: ["cor", "tamanho", "tipo"],
+  carvao: ["kit", "tipo"],
+  aluminio: ["kit", "tipo"],
+  outros: ["especificacao"],
+} as const;

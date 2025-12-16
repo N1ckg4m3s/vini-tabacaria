cbc65582ed923d971096b5c9807419a1294e7d59
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-12-16 20:20:57 -0300

    [ REFAC ] Modificando endpoints para o novo modelo

---
+++ src/app/api/(public)/produto/get-by-relation/route.ts
@@ -0,0 +1,23 @@
+import { BadRequestError } from "@/http/erros.handle";
+import { createRoute } from "@/http/http.handler"
+import { ProductInfoService } from "@/server/productInformations/product.service"
+import { resolveProductId } from "@/server/products/product-id.resolver"
+
+export const GET = createRoute(async (request) => {
+    const { searchParams } = request.nextUrl;
+
+    const productId = await resolveProductId(request)
+
+    const relacao = searchParams.get("relacao");
+    if (!relacao) throw new BadRequestError("Relação entre produtos não mencionada");
+
+    const service = new ProductInfoService()
+
+    if(relacao=='marca'){
+        return await service.obterItensPorMarca({productId})
+    }else if(relacao=='relevancia'){
+        return await service.obterItensPorRelevancia({productId})
+    }else{
+        throw new BadRequestError("Relação não reconhecida")
+    }
+})

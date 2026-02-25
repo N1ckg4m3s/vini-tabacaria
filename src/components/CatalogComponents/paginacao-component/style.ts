7eb02ef69aaed4323a271050d5aab9eaa0397bba
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-02-25 19:04:48 -0300

    [ REFAC ] Migrando de importação de theme antigo

--- src/components/CatalogComponents/paginacao-component/style.ts
+++ src/components/CatalogComponents/paginacao-component/style.ts
@@ -1,12 +1,11 @@
 import { buttonReset, flexCenter } from "@/styles/mixins";
-import { Theme } from "@/styles/theme";
+import { GlobalColors } from "@/styles/theme";
 import styled from "styled-components";
 
 export const PaginacaoContainer = styled.div`
     width: 100%;
     height: 35px;
     padding: 5px;
-    /* background-color: red; */
     ${flexCenter}
     gap: 5px;
 `;
@@ -17,22 +16,25 @@ export const BotaoModificacaoPagina = styled.button<{ $ePaginaAtual?: boolean }>
   height: 100%;
   aspect-ratio: 1/1;
   border-radius: 10px;
-  background-color: transparent;
+  background-color: ${({ $ePaginaAtual }) => (!$ePaginaAtual ? 'transparent' : GlobalColors.Neutral[700])};
   transition: background-color 0.1s;
+    
+  color: ${({ $ePaginaAtual }) => (
+    $ePaginaAtual ?
+      GlobalColors.Text.primary :
+      GlobalColors.Text.muted
+  )};
 
   &:hover {
-    background-color: ${Theme.colors.Fundo.cinzaEscuro};
+    background-color: ${GlobalColors.Neutral[700]};
+    color: ${GlobalColors.Text.primary}
   }
 
   &:disabled {
     cursor: not-allowed;
     opacity: ${({ $ePaginaAtual }) => ($ePaginaAtual ? 1 : 0.6)};
   }
-  
-  color: ${({ $ePaginaAtual }) => (Theme.colors.Texto[$ePaginaAtual ? 'black' : 'white'])};
-  background-color: ${({ $ePaginaAtual }) => (!$ePaginaAtual ? 'transparent' : Theme.colors.Fundo.CinzaClaro)};
 `;
-
 
 export const RetisenciasContainer = styled.div`
   height: 100%;

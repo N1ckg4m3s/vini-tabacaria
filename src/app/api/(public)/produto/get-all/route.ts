9d429c1d0a0660c520d7da80ee69f9027bf67e78
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-02-27 16:30:59 -0300

    [ FEAT ] Implementando no endpoint a função de pesquisa

--- src/app/api/(public)/produto/get-all/route.ts
+++ src/app/api/(public)/produto/get-all/route.ts
@@ -7,12 +7,14 @@ export const GET = createRoute(async (request) => {
     const page = Math.max(Number(searchParams.get("page") || 1), 1);
     const perPage = Math.max(Number(searchParams.get("limit_per_page") || 10), 1);
     const filters = JSON.parse(searchParams.get("filters") || "{}");
+    const search = searchParams.get("search") || ""
 
     const service = new catalogService();
 
     return service.obterItensPorPagina({
         page,
         perPage,
-        filters
+        filters,
+        search
     });
 });

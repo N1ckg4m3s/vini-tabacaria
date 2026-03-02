f94abac4f772f684de3615e314e1f8c9c236747b
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-02 15:05:16 -0300

    Removendo console.log desnecessario

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,8 +41,6 @@ export async function middleware(req: NextRequest) {
 
     const token = obterToken(req);
 
-    console.log('token:', token)
-
     try {
         if (!token) throw new Error('Token não encontrado');
 

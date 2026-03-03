2cfc5af226f123223bdb62c9be9f4c2e62d139d4
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-03 20:01:57 -0300

    [ INFRA ] Espondo as features nas paginas

--- src/app/(admin)/admin/product/new/page.tsx
+++ src/app/(admin)/admin/product/new/page.tsx
@@ -1,5 +1,9 @@
-export default () => {
-    return (<>
-        Register :)
-    </>)
-}
+import { ManageProduct } from "@/features/admin/manageProduct/page/manage.page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Adicionando produto",
+    description: "Catalogo da Vini Tabacaria de Carapicuiba",
+};
+
+export default () => <ManageProduct />

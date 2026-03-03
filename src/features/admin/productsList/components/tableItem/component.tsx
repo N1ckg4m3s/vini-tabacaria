31e97f7dd534bb2f10517c5dda9fba0d001cce9f
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-03 20:00:05 -0300

    [ FEAT ] Adicionando redirecionador

--- src/features/admin/productsList/components/tableItem/component.tsx
+++ src/features/admin/productsList/components/tableItem/component.tsx
@@ -1,18 +1,12 @@
 import { formatePrice } from '@/features/_shered/services/formaters/price.formater'
 import * as s from './style'
 import { Produto } from "@/shered/shered.types"
-
-/*
-<tr>
-    <s.th>Produto</s.th>
-    <s.th>Marca</s.th>
-    <s.th>Tipo</s.th>
-    <s.th>Preço</s.th>
-    <s.th>Ações</s.th>
-</tr>
-*/
+import { useRouter } from 'next/navigation';
 
 export const ProductItem: React.FC<{ produto: Produto }> = ({ produto }) => {
+    const router = useRouter();
+    const navigateToEdit = () => router.push(`/admin/product/${produto.id}`);
+
     return (
         <s.trBody>
             <s.td>{produto.nome}</s.td>
@@ -20,7 +14,7 @@ export const ProductItem: React.FC<{ produto: Produto }> = ({ produto }) => {
             <s.td>{produto.tipo}</s.td>
             <s.td>{formatePrice(produto.valor)}</s.td>
             <s.td>
-                <s.tdActionButton>Editar</s.tdActionButton>
+                <s.tdActionButton onClick={navigateToEdit}>Editar</s.tdActionButton>
             </s.td>
         </s.trBody>
     )

781f64ce802d496190adb7fa45a8fa1895912953
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-18 12:28:23 -0300

    [ FEAT ] Adicionando suporte para Vercel Analytics e Speed Insights

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import '@style/globalStyleCss.css'
 import ClientLayout from './ClientLayout';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "VINI TABACARIA",
@@ -11,6 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClientLayout>
       {children}
+      <Analytics />
+      <SpeedInsights />
     </ClientLayout>
   );
 }

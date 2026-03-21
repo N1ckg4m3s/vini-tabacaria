96c8f5055b21c14f2d830c4556c74ac8a7031ee5
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-21 11:36:06 -0300

    [ FEAT ] Adicionando serviço 'server-side' para obter o order por id

---
+++ src/features/core/order/service/getOrderById.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { OrderService } from "../../../../server/order/order.service"
+import { Order } from "../../../../shered/shered.types"
+
+export const getOrderById = async (orderId: string): Promise<Order> => {
+    const service = new OrderService()
+
+    const order: Order = await service.getOrderById(orderId)
+
+    return order
+}

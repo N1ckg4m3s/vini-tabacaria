752f1d4655a2da7e317263fe5bad04563cca0800
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-03-27 13:25:13 -0300

    [ FEAT ] Adicionando componente para o resumo dos pedidos

---
+++ src/features/admin/orderList/components/orderCard/component.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { formatePrice } from '../../../../_shered/services/formaters/price.formater'
+import { orderResume } from '../../types/type'
+import * as s from './style'
+
+interface props {
+    order: orderResume
+}
+
+export const OrderCardResume: React.FC<props> = ({ order }) => {
+
+    const renderOrderDate = () => {
+        const day = String(order.created_at.getDate()).padStart(2, '0');
+        const month = String(order.created_at.getMonth() + 1).padStart(2, '0');
+        const hour = String(order.created_at.getHours()).padStart(2, '0');
+        const min = String(order.created_at.getMinutes()).padStart(2, '0');
+
+        return `${day}/${month} - ${hour}:${min}`;
+    };
+
+    const renderOrderQuant = () => {
+        const amount = order.order_products_count;
+        const text = amount === 1 ? 'produto' : 'produtos';
+        return `${amount} ${text}`;
+    };
+
+    return (
+        <>
+            <s.orderContainer status={order.status}>
+                <s.orderHeader>
+                    <s.orderStatus>{order.status}</s.orderStatus>
+                    <s.orderTotal>{formatePrice(order.total)}</s.orderTotal>
+                </s.orderHeader>
+
+                <s.orderBody>
+                    <s.orderBodySpan>{renderOrderQuant()}</s.orderBodySpan>
+                    <s.orderBodySpan>{renderOrderDate()}</s.orderBodySpan>
+                </s.orderBody>
+
+                <s.orderFooter> {order.id.slice(0, 10)} </s.orderFooter>
+            </s.orderContainer>
+        </>
+    )
+}
---
+++ src/features/admin/orderList/components/orderCard/style.ts
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { GlobalColors } from "../../../../../styles/theme";
+import { flexCenter, flexColumn } from "../../../../../styles/mixins";
+import { OrderStatus } from "../../../../../server/order/order.types";
+
+export const orderContainer = styled.div<{ status: OrderStatus }>`
+    background-color: ${({ status }) => GlobalColors.statusColor[status].bg};
+    border: 1px solid ${({ status }) => GlobalColors.statusColor[status].border};
+    color: ${({ status }) => GlobalColors.statusColor[status].text};
+    border-radius: 8px;
+    padding: 16px;
+
+    ${flexColumn}
+    gap: 12px;
+
+    transition: border 0.2s ease, transform 0.1s ease;
+
+    &:hover{
+        border-color: ${GlobalColors.Border.subtle};
+        transform: translateY(-2px);
+    }
+`
+
+export const orderHeader = styled.div`
+    ${flexCenter}
+    justify-content: space-between;
+`
+
+export const orderStatus = styled.span`
+    font-size: 13px;
+    color: ${GlobalColors.Text.secondary};
+`
+
+export const orderTotal = styled.span`
+    font-size: 18px;
+    font-weight: bold;
+    color: ${GlobalColors.Text.focus};
+`
+
+export const orderBody = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    color: ${GlobalColors.Text.muted};
+`
+
+export const orderBodySpan = styled.span`
+`
+
+export const orderFooter = styled.div`
+    font-size: 12px;
+    color: ${GlobalColors.Neutral[400]};
+`
+
+
+
+/*
+
+.order-card.pending {
+    background-color: rgba(107, 107, 107, 0.15);
+}
+
+.order-card.handling {
+    background-color: rgba(15, 98, 254, 0.15);
+}
+
+.order-card.completed {
+    background-color: rgba(37, 162, 73, 0.15);
+}
+
+.order-card.canceled {
+    background-color: rgba(218, 30, 40, 0.15);
+}
+*/

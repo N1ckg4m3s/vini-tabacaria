81f545840cb3b6d30caf137383a85f5f042e8de4
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-04-05 20:04:38 -0300

    [ FIX ] Adicionando o Skeleton no retorno

--- src/features/admin/dashboard/generalInformations/geral.loading.page copy.tsx
+++ src/features/admin/dashboard/generalInformations/geral.loading.page copy.tsx
@@ -1,7 +1,5 @@
-import { DashboardService } from '../../../../server/dashboard/dashboard.service';
 import { GlobalColors } from '../../../../styles/theme';
-import { formatePrice } from '../../../_shered/services/formaters/price.formater';
-import { DashboardCard } from './components/card/card.component';
+import { DashboardCardSkeleton } from './components/card/card.component';
 
 /* CSS style */
 const sectionStyle: React.CSSProperties = {
@@ -22,42 +20,21 @@ const cardsContainerStyle: React.CSSProperties = {
     gap: '20px',
     flexWrap: 'wrap',
     gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-
 };
 
-export const DashboardGeralViewLoading = async () => {
-    const service = new DashboardService();
-    const { todayAcess, todayOrders, todayRevenue } = await service.getGeralInfos();
-
-    const weekConversion = todayAcess > 0 ? (todayOrders / todayAcess) * 100 : 0;
-
+export const DashboardGeralViewLoading = async () => {   
     return (
         <section style={sectionStyle}>
             <h2 style={titleStyle}>Visão geral</h2>
             <div style={cardsContainerStyle}>
-                <DashboardCard
-                    title='Receitas Hoje'
-                    value={formatePrice(todayRevenue)}
-                    obs='comparando a ontem'
-                />
 
-                <DashboardCard
-                    title='Pedidos Hoje'
-                    value={todayOrders.toString()}
-                    obs='total de pedidos'
-                />
+                <DashboardCardSkeleton/>
 
-                <DashboardCard
-                    title='Conversão (7 dias)'
-                    value={`${weekConversion.toFixed(2)}%`}
-                    obs='pedidos / acessos'
-                />
+                <DashboardCardSkeleton/>
 
-                <DashboardCard
-                    title='Acessos Hoje'
-                    value={todayAcess.toString()}
-                    obs='última atualização agora'
-                />
+                <DashboardCardSkeleton/>
+
+                <DashboardCardSkeleton/>
             </div>
         </section>
     );

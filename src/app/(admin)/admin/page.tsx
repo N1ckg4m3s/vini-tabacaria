b7610db2a61ac422d06170a5de3ff6e704c40328
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-04-05 22:25:53 -0300

    [ FEAT ] Refatorando dashboard para usar Suspense para carregar cada modulo separadamente

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,5 +1,42 @@
-import { DashboardPage } from "@/features/admin/dashboard/pages/dashboard.page"
+import { CSSProperties, Suspense } from "react";
+import { GlobalColors } from "../../../styles/theme";
+import { DashboardGeralView } from "../../../features/admin/dashboard/generalInformations/dashboard.geral.page";
+import { DashboardTendencia } from "../../../features/admin/dashboard/tendency/dashboard.tendencia.page";
+import { DashboardFunil } from "../../../features/admin/dashboard/funel/dashboard.funil.page";
+import { DashboardTables } from "../../../features/admin/dashboard/products_table/dashboard.tables.page";
+import { DashboardFunilLoading } from "../../../features/admin/dashboard/funel/funil.loading.page";
+import { DashboardGeralViewLoading } from "../../../features/admin/dashboard/generalInformations/geral.loading.page copy";
+import { DashboardTendenciaLoading } from "../../../features/admin/dashboard/tendency/tendencia.loading.page";
+import { DashboardTablesSkeleton } from "../../../features/admin/dashboard/products_table/tables.loading.page";
 
-export default function Page() {
-    return (<DashboardPage />)
+// container
+const dashboardContainer: CSSProperties = {
+    padding: '24px',
+    display: 'grid',
+    gridTemplateRows: 'auto auto auto',
+    gap: '24px',
+
+    color: GlobalColors.Text.primary
+}
+
+export default async function Page() {
+    return <>
+        <div style={dashboardContainer}>
+            <Suspense fallback={<DashboardGeralViewLoading />}>
+                <DashboardGeralView />
+            </Suspense>
+
+            <Suspense fallback={<DashboardTendenciaLoading />}>
+                <DashboardTendencia />
+            </Suspense>
+
+            <Suspense fallback={<DashboardFunilLoading />}>
+                <DashboardFunil />
+            </Suspense>
+
+            <Suspense fallback={<DashboardTablesSkeleton />}>
+                <DashboardTables />
+            </Suspense>
+        </div>
+    </>
 }

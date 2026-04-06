33c37cb9a03dbec1cd8c4d241b188c6848e7ab9e
n1ckg4m3s <nico.ag.03@hotmail.com>
2026-04-05 22:24:36 -0300

    [ INFRA ] Removendo endpoints de resposta do dashboard

--- src/app/api/(admin)/admin/dashboard/productInsites/route.ts
+++
@@ -1,8 +0,0 @@
-import { createRoute } from "@/http/http.handler"
-import { DashboardService } from "@/server/dashboard/dashboard.service";
-
-export const GET = createRoute(async () => {
-    const service = new DashboardService()
-
-    return await service.obterProdutosInsites();
-})
